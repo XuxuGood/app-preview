95230acb94b6a05e88413986fb7aed23e1fffa2f
xuxu03 <xuxu03@corp.netease.com>
2024-03-14 23:30:46 +0800

    优化

--- app-preview-push/src/main/java/com/netease/cloud/dao/TestMapper.java
+++ app-preview-push/src/main/java/com/netease/cloud/dao/TestMapper.java
@@ -2,6 +2,8 @@ package com.netease.cloud.dao;
 
 import com.netease.cloud.model.Order;
 import com.netease.cloud.model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,11 +13,15 @@ import java.util.List;
  * @Date 2024年03月07日
  * @Version: 1.0
  */
+@Mapper
 @Repository
 public interface TestMapper {
 
     List<User> allUserList();
+
     List<User> allUserList1();
+
+    @Select(value = "select * from user")
     List<User> allUserList2();
 
 }
